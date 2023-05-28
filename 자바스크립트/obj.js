1d0fce7c886f1cc5c5fb881dcee95b0d6467cfb9
seorim <hhyjs2r@gmail.com>
2023-05-28 09:06:21 +0900

    [0527] 존재하지 않는 프로퍼티 접근 test

--- 자바스크립트/obj.js
+++ 자바스크립트/obj.js
@@ -6,4 +6,15 @@ let obj2 = obj1;
  b = 15;
  obj2 = { c: 20, d: 'ddd' };
 
-console.log(obj2 === obj1)
+console.log(obj2 === obj1) //false
+
+console.log(a); // (1) undefined. 값을 대입하지 않은 변수에 접근
+
+ let obj = { a: 1 };
+ console.log(obj.a); // 1
+ console.log(obj.b); // (2) 존재하지 않는 프로퍼티에 접근
+ console.log(b); // c.f) ReferenceError: b is not define
+ console.log('t')
+//  let func = function() { };
+//  let c = func(); // (3) 반환(return) 값이 없으면 undefined를 반환한 것으로 간주.
+//  console.log(c);
