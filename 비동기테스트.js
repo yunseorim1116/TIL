95a34e8c571a7241e4f00d4ca01761759e457fb5
seorim <hhyjs2r@gmail.com>
2023-06-21 12:21:37 +0900

    [0621] 실행 테스트

---
+++ 비동기테스트.js
@@ -0,0 +1,13 @@
+
+console.log('1')
+
+setTimeout(()=>{
+   console.log('bb')
+},1000)
+setTimeout(()=>{
+   console.log('bb')
+},1000)
+setTimeout(()=>{
+   console.log('bb')
+},1000)
+console.log('2')
