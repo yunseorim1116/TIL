1c4aebbddd570de168b8b23fa0cb89cdd622f870
seorim <hhyjs2r@gmail.com>
2023-06-28 12:53:05 +0900

    0628 클로저 은닉화 test

--- 클로저.js
+++ 클로저.js
@@ -1,12 +1,37 @@
-var outer = function(){
-    var a = 1
-    var inner = function(){
-        return ++a
-    }
+// var outer = function(){
+//     var a = 1
+//     var inner = function(){
+//         return ++a
+//     }
 
-    return inner
-}
+//     return inner
+// }
 
-const outer2 = outer()
-console.log(outer2())
-console.log(outer2())
+// const outer2 = outer()
+// console.log(outer2())
+// console.log(outer2())
+
+  var counter = (function() {
+      var privateCounter = 0;
+      function changeBy(val) {
+        privateCounter += val;
+      }
+      return {
+        increment: function() {
+          changeBy(1);
+        },
+        decrement: function() {
+          changeBy(-1);
+        },
+        value: function() {
+          return privateCounter;
+        }
+      };
+    })();
+
+    console.log(counter)
+    counter.increment();//1
+    counter.increment();//2
+
+    console.log(counter.privateCounter)
+    console.log(counter.value())
