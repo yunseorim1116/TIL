20a33c3296f6fd0731928daeec6a33f44ebb5824
seorim <hhyjs2r@gmail.com>
2023-07-04 18:45:57 +0900

    0704 커링 화살표 함수 만들어보기

--- 커링.js
+++ 커링.js
@@ -7,6 +7,9 @@ const currying = function(a){
 // const result = currying(1)(2)
 // console.log(result)
 const curryingEx = currying(2)
-console.log(curryingEx)
-console.log(curryingEx(10))
+// console.log(curryingEx)
+// console.log(curryingEx(10))
 //여러 개의 인자를 받는 함수를 하나의 인자만 받는 함수로 나눠서 순차적으로 호출 될 수 있게 체인 형태로 구성한 함수.
+
+const arroutCurrying = a => b => a+b
+console.log(arroutCurrying(5)(8))
