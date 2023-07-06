48374a3fc5dfb33a2c5f641e5e1fd8ec12dad8c0
seorim <hhyjs2r@gmail.com>
2023-07-07 00:02:20 +0900

    0707 private, public 생성자로 만들어보기

--- 생성자.js
+++ 생성자.js
@@ -16,3 +16,39 @@ User.prototype.showName = function(){
 }
 
 console.dir(x.prototype)
+
+function Person(name, age) {
+  // private 변수
+  var privateVariable = "This is private";
+
+  // public 프로퍼티
+  this.name = name;
+  this.age = age;
+
+  // public 메소드
+  this.sayHello = function() {
+    console.log("Hello, my name is " + this.name);
+  };
+
+  // private 메소드
+  function privateMethod() {
+    console.log(privateVariable);
+  }
+
+  // public 메소드
+  this.publicMethod = function() {
+    privateMethod();
+  };
+}
+
+// Person 객체의 인스턴스 생성
+var person1 = new Person("Alice", 25);
+
+console.log(person1.name); // 외부에서 접근 가능
+console.log(person1.age); // 외부에서 접근 가능
+person1.sayHello(); // 외부에서 접근 가능
+
+console.log(person1.privateVariable); // undefined - 외부에서 접근 불가능
+person1.privateMethod(); // TypeError: person1.privateMethod is not a function - 외부에서 접근 불가능
+
+person1.publicMethod(); // 내부에서 접근 가능
