ec84dec18bf962f7b47f17b089c7d97776bfd482
seorim <hhyjs2r@gmail.com>
2023-07-10 13:36:27 +0900

    0710 copy object 함수로 빼 기

--- 깊은복사구현.js
+++ 깊은복사구현.js
@@ -1,3 +1,4 @@
+
 const copyArray = (arr, seen) => {
   return arr.map((item) => copyDeepObject(item, seen));
 };
@@ -26,10 +27,25 @@ const copyMap = (map, seen) => {
   return copiedMap;
 };
 
+const copySymbol = (symbol) => {
+ const { description } = symbol;
+    return Symbol.for(description);
+}
 
+const copyObject = (object, seen)=>{
+   const resultObj = {};
+    for (let key in object) {
+      if (!object.hasOwnProperty(key))  return
+      resultObj[key] = copyDeepObject(object[key], seen);
+    }
+     return resultObj;
+}
 
 const copyDeepObject = (target, seen = new Set()) => {
-  if (Array.isArray(target)) {
+
+  if (typeof target === 'object' && target !== null) { //참조타입
+
+   if (Array.isArray(target)) {
     return copyArray(target, seen);
   }
 
@@ -49,35 +65,32 @@ const copyDeepObject = (target, seen = new Set()) => {
     return copyMap(target, seen);
   }
 
-  if (typeof target === 'object' && target !== null) {
-
     if (seen.has(target)) {
+      console.log('seen')
+      console.log(seen)
+      console.log('target')
+      console.log(target)
       return target;
     }
 
     seen.add(target);
+    copyObject(target, seen)
 
-    const resultObj = {};
+  } else { //원시타입
 
-    for (let key in target) {
-      if (target.hasOwnProperty(key)) {
-        resultObj[key] = copyDeepObject(target[key], seen);
-      }
-    }
-    return resultObj;
-  }
-
-  if (typeof target === 'symbol') {
-    const { description } = target;
-    return Symbol.for(description);
+   if (typeof target === 'symbol') {
+     return copySymbol(target)
   }
 
   //객체도, 예외 객체도, Symbol타입도 아닐 때
   return target;
+  }
+
+
 };
 
-const sym =  Symbol("g")
-const objA = { strA:'A', a :'a', g: sym};
+
+const objA = { strA:'A', a :'a', g: Symbol("g")};
 const objB = { strB:'B' , b:'b'};
 
 console.log(objA)
@@ -89,7 +102,7 @@ console.log(objA)
 const copiedObj = copyDeepObject(objA);
 
 console.log('---')
-console.log(copiedObj.g === objA.g)
+
 
 // const x = { a: '1', g: Symbol("g") , b:[1,2,3] }
 // const a = copyDeepObject(x)
