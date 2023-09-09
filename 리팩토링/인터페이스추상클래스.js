82dd939999ac083cc6984b5d8e6019cc340bc08c
seorim <hhyjs2r@gmail.com>
2023-09-09 23:58:25 +0900

    0909 리팩토링

---
+++ 리팩토링/인터페이스추상클래스.js
@@ -0,0 +1,19 @@
+function updateTile(x, y) {
+  if (
+    (map[y][x].isStone() || map[y][x].isFallingStone()) &&
+    map[y + 1][x].isAair()
+  ) {
+    map[y + 1][x] = new FallingStone();
+    map[y][x] = new Air();
+  } else if (
+    (map[y][x].isBox() || map[y][x].isFallingBox()) &&
+    map[y + 1][x].isAir()
+  ) {
+    map[y + 1][x] = new FallingBox();
+    map[y][x] = new Air();
+  } else if (map[y][x].isFallingStone()) {
+    map[y][x] = new Stone();
+  } else if (map[y][x].isFallingBox()) {
+    map[y][x] = new Box();
+  }
+}
